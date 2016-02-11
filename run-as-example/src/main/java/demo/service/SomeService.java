80dacc66ac66a2078395b6ca856bb2ea1e2c83ed
Ray Suliteanu <raysuliteanu@fico.com>
2016-02-11 14:41:54 -0800

    change log message to an assertion

--- run-as-example/src/main/java/demo/service/SomeService.java
+++ run-as-example/src/main/java/demo/service/SomeService.java
@@ -1,25 +1,34 @@
 package demo.service;
 
+import java.util.Arrays;
+
 import demo.data.Account;
 import demo.repository.AccountRepository;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 @Service
 public class SomeService {
-    private static final Logger LOG = LoggerFactory.getLogger(SomeService.class);
-
     @Autowired
     private AccountRepository accountRepository;
 
     @PreAuthorize("hasAnyRole('RUN_AS_ADMIN', 'ADMIN')")
     public Account doSomething(final Account account) {
-        LOG.info("should have ROLE_RUN_AS_ADMIN and/or ROLE_ADMIN: {}", SecurityContextHolder.getContext().getAuthentication());
+        if (SecurityContextHolder.getContext() != null) {
+            final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
-        return account;
+            assert authentication != null &&
+                    CollectionUtils.containsAny(
+                            authentication.getAuthorities(),
+                            Arrays.asList(new SimpleGrantedAuthority("RUN_AS_ADMIN"), new SimpleGrantedAuthority("ADMIN"))
+                    );
+        }
+
+        return accountRepository.save(account);
     }
 }
