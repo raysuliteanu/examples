836fe176425ec862d78590920af05735e18ab4de
Ray Suliteanu <raysuliteanu@fico.com>
2016-02-11 09:31:56 -0800

    initial import of basic working example
    fully working example with 'run as' support using vanilla spring security implementation
    
    id should be generated
    
    createDate should be automatically set
    
    add some finder methods
    
    change package root to just be 'demo'
    
    improve example to have 2 uses, one with run as, one without

---
+++ src/main/java/demo/controller/SomeController.java
@@ -0,0 +1,27 @@
+package demo.controller;
+
+import demo.data.Account;
+import demo.service.SomeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SomeController {
+    @Autowired
+    private SomeService someService;
+
+    @RequestMapping(value = "/something", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.OK)
+    public Account doSomething(@RequestBody final Account account) {
+        return someService.doSomething(account);
+    }
+}
+
