2f2c700525bb4a6d2e36f9ecf6d26ebede2b78fa
Ray Suliteanu <raysuliteanu@gmail.com>
2021-02-03 17:31:20 -0800

    cleanup refactor of the prefix handling

--- algorithms/lib/src/main/java/misc/PrePostInFixNotation.java
+++ algorithms/lib/src/main/java/misc/PrePostInFixNotation.java
@@ -5,63 +5,93 @@ import java.util.Stack;
 import static java.lang.Long.parseLong;
 
 public abstract class PrePostInFixNotation {
+    enum Operator {
+        Multiply,
+        Divide,
+        Mod,
+        Add,
+        Subtract;
+
+        static Operator from(String value) {
+            Operator result = null;
+            switch (value) {
+                case "*" -> result = Multiply;
+                case "/" -> result = Divide;
+                case "+" -> result = Add;
+                case "-" -> result = Subtract;
+                case "%" -> result = Mod;
+            }
+            return result;
+        }
+    }
+
     public static Long evalPrefixExpression(final String expression) {
-        final Stack<String> operators = new Stack<>();
+        final Stack<Operator> operators = new Stack<>();
         final Stack<Long> operands = new Stack<>();
         String[] tokens = expression.split(" ");
 
-        if (!isOperator(tokens[0])) {
-            throw new IllegalArgumentException("invalid expression " + expression);
+        if (!isOperator(Operator.from(tokens[0]))) {
+            illegalExpression(expression);
         }
 
-        String lastToken = "";
+        Operator lastToken = null;
         for (String token : tokens) {
-            if (isOperator(token)) {
-                operators.push(token);
+            final Operator operator = Operator.from(token);
+            if (isOperator(operator)) {
+                // defer till we have operands
+                operators.push(operator);
             }
             else if (isOperand(token)) {
                 if (!isOperator(lastToken)) {
-                    String operator = operators.pop();
-                    Long right = parseLong(token);
-                    Long left = operands.pop();
-                    operands.push(evaluate(operator, left, right));
+                    processOperator(operators, operands, parseLong(token));
                 }
                 else {
+                    // defer till we have enough operands
                     operands.push(parseLong(token));
                 }
             }
             else {
-                throw new IllegalArgumentException("invalid expression " + expression);
+                illegalExpression(expression);
             }
 
-            lastToken = token;
+            lastToken = operator;
         }
 
+        // process remaining operators and operands if any
         while (!operators.isEmpty()) {
-            String operator = operators.pop();
-            Long right = operands.pop();
-            Long left = operands.pop();
-            operands.push(evaluate(operator, left, right));
+            processOperator(operators, operands, operands.pop());
         }
 
+        // ultimately, after all operators processed, should be left with a single value on the operand stack, our result
         if (!(operands.size() == 1)) {
-            throw new IllegalArgumentException("invalid expression " + expression);
+            illegalExpression(expression);
         }
 
         return operands.pop();
     }
 
-    private static Long evaluate(final String operator, final Long left, final Long right) {
+    private static void processOperator(final Stack<Operator> operators, final Stack<Long> operands, final long rightOperand) {
+        Operator operator = operators.pop();
+        Long right = rightOperand;
+        Long left = operands.pop();
+        operands.push(evaluate(operator, left, right));
+    }
+
+    private static Long evaluate(final Operator operator, final Long left, final Long right) {
         long result = 0L;
         switch (operator) {
-            case "*" -> result = (left * right);
-            case "/" -> result = (left / right);
-            case "+" -> result = (left + right);
-            case "-" -> result = (left - right);
-            case "%" -> result = (left % right);
+            case Multiply -> result = (left * right);
+            case Divide -> result = (left / right);
+            case Add -> result = (left + right);
+            case Subtract -> result = (left - right);
+            case Mod -> result = (left % right);
         }
 
         return result;
+    }
+
+    private static void illegalExpression(final String expression) {
+        throw new IllegalArgumentException("invalid expression " + expression);
     }
 
     private static boolean isOperand(final String token) {
@@ -74,11 +104,7 @@ public abstract class PrePostInFixNotation {
         }
     }
 
-    private static boolean isOperator(final String token) {
-        return token.equals("*") ||
-                token.equals("/") ||
-                token.equals("+") ||
-                token.equals("-") ||
-                token.equals("%");
+    private static boolean isOperator(final Operator operator) {
+        return operator != null;
     }
 }
