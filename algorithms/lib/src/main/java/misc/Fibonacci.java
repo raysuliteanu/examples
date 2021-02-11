05766014998c4354828dbc503ef992a8f4fda446
Ray Suliteanu <raysuliteanu@gmail.com>
2021-02-11 10:13:01 -0800

    add fibonacci example with 3 impls - recursive, bottom up, recursive with memoization

---
+++ algorithms/lib/src/main/java/misc/Fibonacci.java
@@ -0,0 +1,40 @@
+package misc;
+
+import java.util.concurrent.ConcurrentHashMap;
+
+public abstract class Fibonacci {
+    public static int fib(int value) {
+        if (value <= 1) {
+            return value;
+        }
+
+        return fib(value - 1) + fib(value - 2);
+    }
+
+    public static int fibBU(int value) {
+        if (value == 0) {
+            return 0;
+        }
+
+        int previousFib = 0;
+        int currentFib = 1;
+        for (int i = 0; i < value - 1; i++) {
+            int newFib = previousFib + currentFib;
+            previousFib = currentFib;
+            currentFib = newFib;
+        }
+
+        return currentFib;
+    }
+
+    static ConcurrentHashMap<Integer, Integer> memo = new ConcurrentHashMap<>();
+
+    static {
+        memo.put(0, 0);
+        memo.put(1, 1);
+    }
+
+    public static int fibMemo(int value) {
+        return memo.computeIfAbsent(value, n -> fibMemo(n - 1) + fibMemo(n - 2));
+    }
+}
---
+++ algorithms/lib/src/test/java/misc/FibonacciTest.java
@@ -0,0 +1,52 @@
+package misc;
+
+import org.junit.jupiter.api.Test;
+
+import static misc.Fibonacci.fib;
+import static misc.Fibonacci.fibBU;
+import static misc.Fibonacci.fibMemo;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class FibonacciTest {
+    @Test
+    void fibonacciRecursive() {
+        assertEquals(0, fib(0));
+        assertEquals(1, fib(1));
+        assertEquals(1, fib(2));
+        assertEquals(2, fib(3));
+        assertEquals(3, fib(4));
+        assertEquals(5, fib(5));
+        assertEquals(8, fib(6));
+        assertEquals(13, fib(7));
+        assertEquals(21, fib(8));
+        assertEquals(34, fib(9));
+    }
+
+    @Test
+    void fibonacciBottomUp() {
+        assertEquals(0, fibBU(0));
+        assertEquals(1, fibBU(1));
+        assertEquals(1, fibBU(2));
+        assertEquals(2, fibBU(3));
+        assertEquals(3, fibBU(4));
+        assertEquals(5, fibBU(5));
+        assertEquals(8, fibBU(6));
+        assertEquals(13, fibBU(7));
+        assertEquals(21, fibBU(8));
+        assertEquals(34, fibBU(9));
+    }
+
+    @Test
+    void fibonacciMemo() {
+        assertEquals(0, fibMemo(0));
+        assertEquals(1, fibMemo(1));
+        assertEquals(1, fibMemo(2));
+        assertEquals(2, fibMemo(3));
+        assertEquals(3, fibMemo(4));
+        assertEquals(5, fibMemo(5));
+        assertEquals(8, fibMemo(6));
+        assertEquals(13, fibMemo(7));
+        assertEquals(21, fibMemo(8));
+        assertEquals(34, fibMemo(9));
+    }
+}
