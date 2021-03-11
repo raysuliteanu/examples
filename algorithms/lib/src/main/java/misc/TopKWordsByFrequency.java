e3bfc6d786b0ac45fea718a66c1bea353db4ebf5
Ray Suliteanu <raysuliteanu@gmail.com>
2021-03-11 11:25:02 -0800

    refine TopK example

--- algorithms/lib/src/main/java/misc/TopKWordsByFrequency.java
+++ algorithms/lib/src/main/java/misc/TopKWordsByFrequency.java
@@ -1,8 +1,8 @@
 package misc;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static java.lang.Long.compare;
 import static java.util.stream.Collectors.counting;
@@ -11,40 +11,16 @@ import static java.util.stream.Collectors.toList;
 
 public class TopKWordsByFrequency {
     public List<String> topKFrequent(String[] words, int k) {
-
-        final List<Tuple> list = Arrays.stream(words)
+        return Arrays.stream(words)
                 .collect(groupingByConcurrent(word -> word, counting()))
-                .entrySet().parallelStream()
-                .map(entry -> Tuple.of(entry.getValue(), entry.getKey()))
-                .sorted()
+                .entrySet().stream()
+                .sorted((o1, o2) -> {
+                    final int compareCount = compare(o2.getValue(), o1.getValue());
+                    // if same count, sort/order by word
+                    return compareCount != 0 ? compareCount : o1.getKey().compareTo(o2.getKey());
+                })
+                .limit(k)
+                .map(Map.Entry::getKey)
                 .collect(toList());
-
-        final List<String> topK = new ArrayList<>(k);
-        for (int i = 0; i < k && !list.isEmpty(); i++) {
-            topK.add(list.remove(0).word);
-        }
-
-        return topK;
-    }
-
-    private static class Tuple implements Comparable<Tuple> {
-        long count;
-        String word;
-
-        public Tuple(long count, String word) {
-            this.count = count;
-            this.word = word;
-        }
-
-        public static Tuple of(long count, String word) {
-            return new Tuple(count, word);
-        }
-
-        @Override
-        public int compareTo(final Tuple other) {
-            final int compareCount = compare(other.count, count);
-            // if same count, sort/order by word
-            return compareCount != 0 ? compareCount : word.compareTo(other.word);
-        }
     }
 }
