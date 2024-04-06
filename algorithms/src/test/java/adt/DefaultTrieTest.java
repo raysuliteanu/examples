61491829f61e7f523b328ad86fef27f4ecbc6d02
Ray Suliteanu <raysuliteanu@gmail.com>
2024-04-06 12:30:52 -0700

    Refactor TrieNode operations in DefaultTrie class
    The TrieNode operations such as insert, find, delete, and matches are moved to the DefaultTrie class from the inner TrieNode class. This change makes the code clearer and easy to understand by fixing where methods are implemented. It also includes the update of assertion imports in DefaultTrieTest class.

--- algorithms/src/test/java/adt/DefaultTrieTest.java
+++ algorithms/src/test/java/adt/DefaultTrieTest.java
@@ -6,10 +6,7 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class DefaultTrieTest {
     @Test
@@ -52,11 +49,6 @@ class DefaultTrieTest {
     @Test
     public void nullWordMatches() {
         assertThrows(IllegalArgumentException.class, () -> new DefaultTrie().matches(null));
-    }
-
-    @Test
-    public void findNullWord() {
-        assertFalse(new DefaultTrie().find(null));
     }
 
     @Test
